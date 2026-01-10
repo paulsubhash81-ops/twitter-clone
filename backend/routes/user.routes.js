4dbb7b4017346ad5536fa8b83c960b210c70d7a7
paulsubhash81-ops <paulsubhash81@gmail.com>
2026-01-11 01:26:07 +0530

    1. user routes and controllers added

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+import {
+  followUnfollowUser,
+  getSuggestedUsers,
+  getUserProfile,
+  updateUser,
+} from "../controllers/user.controller.js";
+import { get } from "mongoose";
+
+const router = express.Router();
+
+router.get("/profile/:username", protectRoute, getUserProfile);
+router.get("/suggested", protectRoute, getSuggestedUsers);
+router.post("/follow/:id", protectRoute, followUnfollowUser);
+router.post("/update", protectRoute, updateUser);
+
+export default router;
