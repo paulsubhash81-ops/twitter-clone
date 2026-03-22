01b90713bb4065a6a1bdabc352e4f317c4ae19ec
paulsubhash81-ops <paulsubhash81@gmail.com>
2026-03-22 19:17:39 +0530

    tiny fixes updated using comment

--- frontend/src/pages/auth/signup/SignUpPage.jsx
+++ frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -7,7 +7,7 @@ import { MdOutlineMail } from "react-icons/md";
 import { FaUser } from "react-icons/fa";
 import { MdPassword } from "react-icons/md";
 import { MdDriveFileRenameOutline } from "react-icons/md";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 
 const SignUpPage = () => {
@@ -17,6 +17,8 @@ const SignUpPage = () => {
     fullName: "",
     password: "",
   });
+
+  const queryClient = useQueryClient();
 
   const { mutate, isError, isPending, error } = useMutation({
     mutationFn: async ({ email, username, fullName, password }) => {
@@ -39,6 +41,10 @@ const SignUpPage = () => {
     },
     onSuccess: () => {
       toast.success("Account created successfully!");
+      {
+        // Invalidate the authUser query to refetch the authenticated user data, which will update the UI accordingly.
+        queryClient.invalidateQueries({ queryKey: ["authUser"] });
+      }
     },
   });
 
